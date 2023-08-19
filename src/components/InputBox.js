9b4ed11a5a385a90f93bfd306eaaae579080e176
Minho Lim <minho119@ajou.ac.kr>
2023-08-19 22:17:57 +0900

    feat: add InputForm & FullButton component

---
+++ src/components/InputBox.js
@@ -0,0 +1,85 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Family, Label } from '../styles/fonts';
+import { forwardRef, useState } from 'react';
+import { DANGER, GRAY, PRIMARY } from '../styles/colors';
+
+const InputBox = forwardRef(
+  (
+    {
+      title,
+      placeholder,
+      supportingText,
+      value,
+      errorMessage,
+      marginTop,
+      ...props
+    },
+    ref
+  ) => {
+    const [isFocused, setIsFocused] = useState(false);
+
+    return (
+      <View style={styles.container}>
+        {title ?? <Text style={styles.title}>{title}</Text>}
+
+        <View>
+          <TextInput
+            style={[
+              styles.inputContainer,
+              isFocused && styles.focusedInput,
+              errorMessage && styles.errorInput,
+              { marginTop: marginTop },
+            ]}
+            {...props}
+            ref={ref}
+            value={value}
+            placeholder={placeholder ?? title}
+            placeholderTextColor={GRAY.DEFAULT}
+            selectionColor={isFocused && GRAY.DEFAULT}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+          />
+        </View>
+
+        <Text
+          style={[
+            styles.supportingText,
+            errorMessage && { color: DANGER.DEFAULT },
+          ]}
+        >
+          {errorMessage ? errorMessage : supportingText}
+        </Text>
+      </View>
+    );
+  }
+);
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+  title: {
+    ...Family.KR_Medium,
+    ...Label.Large,
+  },
+  inputContainer: {
+    height: 40,
+    paddingLeft: 8,
+    borderBottomWidth: 1,
+    borderColor: GRAY[300],
+  },
+  focusedInput: {
+    borderColor: PRIMARY.DEFAULT,
+  },
+  errorInput: {
+    borderColor: DANGER.DEFAULT,
+  },
+  supportingText: {
+    ...Family.KR_Medium,
+    ...Label.Medium,
+    color: GRAY.DEFAULT,
+    marginTop: 8,
+  },
+});
+
+export default InputBox;
---
+++ src/components/buttons/FullButton.js
@@ -0,0 +1,29 @@
+import { Pressable, StyleSheet, Text } from 'react-native';
+import { PRIMARY, WHITE } from '../../styles/colors';
+import { Body, Family } from '../../styles/fonts';
+
+const FullButton = ({ title, onPress, disabled }) => {
+  return (
+    <Pressable style={styles.container} onPress={onPress} disabled={disabled}>
+      <Text style={styles.title}>{title}</Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 12,
+    backgroundColor: PRIMARY.DEFAULT,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    marginTop: 16,
+  },
+  title: {
+    ...Family.KR_Medium,
+    ...Body.Large,
+    color: WHITE,
+  },
+});
+
+export default FullButton;
